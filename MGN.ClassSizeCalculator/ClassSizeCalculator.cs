e53bcc7362cc1664b6be2973cfe095562bcd4d0d
Marcus Newman <marcusgeorgenewman@gmail.com>
2016-05-21 16:03:09 -0700

    Initial Check in

---
+++ MGN.ClassSizeCalculator/ClassSizeCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MGN.ClassSizeCalculator
+{
+    public class ClassSizeCalculator
+    {
+        public void Calculate(int[] a)
+        { }
+    }
+}
---
+++ Tests/ClassSizeCalculatorTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+
+namespace Tests
+{
+    [TestClass]
+    public class ClassSizeCalculatorTests
+    {
+        [TestMethod]
+        public void AssemblyShouldExist()
+        {
+            var assembly = GetAssembly();
+            Assert.IsNotNull(assembly, "Assembly should exist and be referenced.");
+        }
+
+        private static Assembly GetAssembly()
+        {
+            Assembly assembly = null;
+            try
+            {
+                assembly = Assembly.Load("MGN.ClassSizeCalculator");
+            }
+            catch { }
+
+            return assembly;
+        }
+
+        [TestMethod]
+        public void ClassSizeCalculatorClassShouldExist()
+        {
+            var type = GetAssembly().GetType("MGN.ClassSizeCalculator.ClassSizeCalculator");
+            Assert.IsNotNull(type, "ClassSizeCalculator class should exist.");
+        }
+
+        [TestMethod]
+        public void CalculateMethodShouldExist()
+        {
+            var methodInfo = GetAssembly().GetType("MGN.ClassSizeCalculator.ClassSizeCalculator").GetMethod("Calculate");
+            Assert.IsNotNull(methodInfo, "Calculate method should exist.");
+        }
+
+        [TestMethod]
+        public void CalculateMethodShouldAcceptAParameter()
+        {
+            var parameters = GetAssembly().GetType("MGN.ClassSizeCalculator.ClassSizeCalculator").GetMethod("Calculate").GetParameters();
+            Assert.AreEqual(1, parameters.Length, "Calculate should accept a parameter.");
+        }
+
+        [TestMethod]
+        public void CalculateMethodShouldAcceptAnIntArray()
+        {
+            var parameter = GetAssembly().GetType("MGN.ClassSizeCalculator.ClassSizeCalculator").GetMethod("Calculate").GetParameters()[0];
+            Assert.AreEqual(parameter.ParameterType, typeof(int[]), "Calculate method should accept an integer array as its first parameter.");
+        }
+    }
+}
